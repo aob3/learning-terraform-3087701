368827cb33bdeabd4bdaa0b7646cd46ee9a9e3e9
aob3 <74620762+aob3@users.noreply.github.com>
2023-06-20 00:28:36 -0400

    version change

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source        = "terraform-aws-modules/security-group/aws"
-  version       = "5.1.0"
+  version       = "3.14.2"
   name          = "blog_new"
 
   vpc_id              = data.aws_vpc.default.id
